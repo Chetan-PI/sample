3622e872ed142255959dd55244189a3db7a55ea8
Chetan-PI <44423997+Chetan-PI@users.noreply.github.com>
2019-09-21 13:45:42 +0530

    Add files via upload

---
+++ Program2.java
@@ -0,0 +1,11 @@
+class Program2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Main method started");
+		final int x=10;
+		System.out.println("x="+x);
+		x=20;
+		System.out.println("Main method ended");
+	}
+}
---
+++ Program3.java
@@ -0,0 +1,26 @@
+class Program3
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Main Method started");
+		int x;
+		double y;
+		float f;
+		long l;
+		boolean b;
+		String s;
+		x=10;
+		y=3.14;
+		f=434.4f;
+		l=100l;
+		b=true;
+		s="Qspider";
+		System.out.println("x="+x);
+		System.out.println("y="+y);
+		System.out.println("f="+f);
+		System.out.println("l="+l);
+		System.out.println("b="+b);
+		System.out.println("s="+s);
+		System.out.println("Main Method ended");
+	}
+}
